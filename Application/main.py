829c4610ac6986e376b0ba7f11fd01b0709ff338
Matt <mbroadbridge96@gmail.com>
2023-10-27 14:21:06 +0100

    Tidied up and added more functions for accessing SQLite database. Created functionality and UI for adding cards to decks, and viewing all decks.

--- Application/main.py
+++ Application/main.py
@@ -2,24 +2,143 @@
 from PyQt5 import QtGui, QtCore, QtWidgets
 import sys
 from os import path, chdir, mkdir, getlogin
-from Windows.mainWindow import Ui_MainWindow
+
+from PyQt5.QtWidgets import QMessageBox
+
+import Windows.mainWindow
+import Windows.addDeck
+import Windows.viewDeck
+import Windows.editDeck
 from Objects.profile import Profile
 
 
+class MyWindow(QtWidgets.QMainWindow, Windows.mainWindow.Ui_MainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
 
-class MyWindow(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(MyWindow, self).__init__()
 
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
+        self.setupUi(self)
+
+        # This variable holds an instance of an opened window, to stop pythons garbage collection immediately shutting it
+        #self.dialog = None
+
+        # Connecting elements of the UI to functions
+        self.viewBtn.clicked.connect(self.view_decks)
+        #self.RemoveBtn.clicked.connect(self.remove_deck)
+
+        self.profile = Profile(getlogin(), path.join(path.expanduser("~"), ".SpaceLearn"))
+
+    def view_decks(self):
+        dialog = ViewDecksWindow(self.profile)
+        dialog.exec()
+
+    def closeEvent(self, event):
+        print("Closing app")
+        self.profile.close_db()
+
+class ViewDecksWindow(QtWidgets.QDialog, Windows.viewDeck.Ui_Dialog):
+    def __init__(self, profile, parent=None):
+        super().__init__(parent)
+
+        self.profile = profile
+
+        self.setupUi(self)
+
+        self.setWindowTitle("View Decks")
+
+        self.update_list()
+
+        self.addBtn.clicked.connect(self.add_deck)
+        self.removeBtn.clicked.connect(self.remove_deck)
+        self.editBtn.clicked.connect(self.edit_deck)
+        self.exitBtn.clicked.connect(self.close)
+
+        self.selected_deck = None
+        self.listWidget.itemClicked.connect(self.list_click)
+
+    def list_click(self, item):
+        print(item.text())
+        self.selected_deck = item.text()
+
+    def update_list(self):
+        self.listWidget.clear()
+        names = self.profile.get_deck_names()
+        for name in names:
+            self.listWidget.insertItem(0, name)    # First argument passed is position in list, this ensures that its always first
+
+    def add_deck(self):
+        dialog = AddDeckWindow(self.profile)
+        dialog.exec()
+        self.update_list()
+
+    def remove_deck(self):
+        if self.selected_deck is not None:
+            query = QtWidgets.QMessageBox
+            res = query.question(self, 'Delete Deck', 'Are you sure you want to delete ' + self.selected_deck + '?', query.Yes | query.No)
+            if res == query.Yes:
+                self.profile.remove_deck(self.selected_deck)
+                self.update_list()
+                self.selected_deck = None       # Failsafe in case selected is not set to None when removed
+
+    def edit_deck(self):
+        if self.selected_deck is not None:
+            dialog = EditDeckWindow(self.profile, self.selected_deck)
+            dialog.exec()
+
+
+class EditDeckWindow(QtWidgets.QDialog, Windows.editDeck.Ui_Dialog):
+    def __init__(self, profile, deck_name, parent=None):
+        super().__init__(parent)
+
+        self.profile = profile
+
+        self.deck_name = deck_name
+
+        self.setupUi(self)
+        self.setWindowTitle(str(deck_name))
+
+        self.addBtn.clicked.connect(self.add_card)
+        self.exitBtn.clicked.connect(self.close)
+
+
+    def add_card(self):
+        self.profile.add_card(self.deck_name, self.questionEdit.toPlainText(), self.answerEdit.toPlainText())
+
+class AddDeckWindow(QtWidgets.QDialog, Windows.addDeck.Ui_Dialog):
+    def __init__(self, profile, parent=None):
+        super().__init__(parent)
+
+        self.profile = profile
+
+        self.setupUi(self)
+
+        self.addDeckBtn.clicked.connect(self.add_deck)
+        self.cancelBtn.clicked.connect(self.close)
+
+    def add_deck(self):
+        res = self.profile.add_deck(self.plainTextEdit.toPlainText())
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        msg.setText(res)
+        msg.setWindowTitle("Message")
+        msg.exec()
+
+
+class RemoveDeckWindow(QtWidgets.QDialog, Windows.addDeck.Ui_Dialog):
+    def __init__(self, profile, parent=None):
+        super().__init__(parent)
+
+        self.profile = profile
+
+        self.setupUi(self)
+
 
 
 def application():
     app = QtWidgets.QApplication(sys.argv)
 
     first_window = MyWindow()
-    first_window.show()
+    first_window.showMaximized()
     # Set window size
     #first_window.resize(400, 300)
 
@@ -32,14 +151,8 @@ def application():
 
 
 def main():
-
-    user = Profile(getlogin(), path.join(path.expanduser("~"), ".SpaceLearn"))
-
-    print(user.add_deck("testdeck"))
-
-    print(user.remove_deck("testdeck"))
-
-    user.close_db()
+    print("App start")
+    application()
 
 
 if __name__ == '__main__':
--- Objects/profile.py
+++ Objects/profile.py
@@ -1,3 +1,5 @@
+import sys
+import traceback
 from os import path, mkdir, getlogin
 import sqlite3
 
@@ -17,28 +19,61 @@ class Profile:
 
     def add_deck(self, deck_title):
 
-        res = self.cur.execute("SELECT ? FROM sqlite_master WHERE type='table' AND name=?", (deck_title, deck_title))
+        sql = "SELECT name FROM sqlite_master WHERE type='table' AND name=" + "'" + scrub(deck_title) + "'"
+
+        res = self.run_sql(sql)
 
         if str(res.fetchone()) == "None":
-            sql_query = "CREATE TABLE " + scrub(deck_title) + "(card, score)"  # Apparently table names cant be parameterised, so scrub the input to prevent sql injections
-            self.cur.execute(sql_query)
+            sql_query = "CREATE TABLE " + scrub(deck_title) + "(id integer PRIMARY KEY, question string, answer string, score integer)"  # Apparently table names cant be parameterised, so scrub the input to prevent sql injections
+            self.run_sql(sql_query)
             return deck_title + " added"
         else:
             return "Error - " + deck_title + " already exists"
 
     def remove_deck(self, deck_title):
 
-        res = self.cur.execute("SELECT ? FROM sqlite_master WHERE name=?", (deck_title, deck_title))
+        sql = "SELECT name FROM sqlite_master WHERE type='table' AND name=" + "'" + scrub(deck_title) + "'"
+
+        res = self.run_sql(sql)
 
         if not str(res.fetchone()) == "None":
             sql_query = "DROP TABLE " + scrub(deck_title)             # Apparently table names cant be parameterised, so scrub the input to prevent sql injections.
-            self.cur.execute(sql_query)                                        # User input is selection from menu, but better safe than sorry
+            self.run_sql(sql_query)                                       # User input is selection from menu, but better safe than sorry
             return deck_title + " removed"
         else:
             return "Error - You shouldn't see this"
 
+    def get_deck_names(self):
+        sql = "SELECT name FROM sqlite_master WHERE type='table'"
+        res = self.run_sql(sql)
+        return [x[0] for x in res.fetchall()]
+
+    def add_card(self, deck_name, question, answer):
+        sql = "INSERT INTO " + scrub(deck_name) + "(question,answer,score) VALUES(?,?,?)"   # Needs parameters passing so running in this function rather than the general SQL run one
+        try:
+            res = self.cur.execute(sql, (question, answer, "PLACEHOLDER"))
+            self.con.commit()
+        except sqlite3.Error as er:
+            print('SQLite error: %s' % (' '.join(er.args)))
+            print("Exception class is: ", er.__class__)
+            print('SQLite traceback: ')
+            exc_type, exc_value, exc_tb = sys.exc_info()
+            print(traceback.format_exception(exc_type, exc_value, exc_tb))
+        return res
+
+
+    def run_sql(self, sql):
+        try:
+            res = self.cur.execute(sql)
+        except sqlite3.Error as er:
+            res = "SQL ERROR"
+            print('SQLite error: %s' % (' '.join(er.args)))
+            print("Exception class is: ", er.__class__)
+            print('SQLite traceback: ')
+            exc_type, exc_value, exc_tb = sys.exc_info()
+            print(traceback.format_exception(exc_type, exc_value, exc_tb))
+        return res
 
     def close_db(self):
         self.cur.close()
         self.con.close()
-
